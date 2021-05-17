b44f0b197146d3bb38aa2167099ef466d2251144
coemans <tom.coemans@zeuzios.be>
2021-05-17 11:17:51 +0200

    Introductie vl-form-group webcomponent (#17)

--- test/e2e/form.test.js
+++ test/e2e/form.test.js
@@ -14,12 +14,8 @@ describe('vl-form', async () => {
 
   it('als gebruiker kan ik een formulier submitten zonder dat de URL aangepast wordt', async () => {
     const form = await vlFormPage.getForm();
-    const emailInput = await new VlInputField(driver, await form.findElement(By.css('#email')));
     const nameInput = await new VlInputField(driver, await form.findElement(By.css('#name')));
-    const surnameInput = await new VlInputField(driver, await form.findElement(By.css('#surname')));
-    await emailInput.setValue('jos.bosmans@vlaanderen.be');
     await nameInput.setValue('Jos');
-    await surnameInput.setValue('Bosmans');
     await form.submit();
     const url = await driver.getCurrentUrl();
     assert.isTrue(url.endsWith('?no-header=true&no-footer=true'));
